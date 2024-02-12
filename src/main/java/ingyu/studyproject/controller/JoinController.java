4560756df0a00ad4b6c97fdbb66728016ecc96b7
InGyu Moon <ansdlsrb1121@naver.com>
2024-02-12 16:12:33 +0900

    feat: contoller 새구현

---
+++ src/main/java/ingyu/studyproject/controller/JoinController.java
@@ -0,0 +1,25 @@
+package ingyu.studyproject.controller;
+
+import ingyu.studyproject.dto.JoinDTO;
+import ingyu.studyproject.service.JoinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class JoinController {
+
+    private final JoinService joinService;
+
+    @GetMapping("/join")
+    public String joinP(){
+        return "/join";
+    }
+    @PostMapping("/joinSuccess")
+    public String joinSuccess(JoinDTO joinDTO){
+        joinService.save(joinDTO);
+        return "redirect:/login";
+    }
+}
