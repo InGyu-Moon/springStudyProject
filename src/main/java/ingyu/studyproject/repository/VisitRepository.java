5da5a27ad2ef60bc414f67272d2ba0be9806df86
ingyu <ansdlsrb1121@naver.com>
2024-02-15 11:52:12 +0900

    feat: VisitRepository 구현

---
+++ src/main/java/ingyu/studyproject/repository/VisitRepository.java
@@ -0,0 +1,7 @@
+package ingyu.studyproject.repository;
+
+import ingyu.studyproject.entity.VisitEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VisitRepository extends JpaRepository<VisitEntity,Long> {
+}
