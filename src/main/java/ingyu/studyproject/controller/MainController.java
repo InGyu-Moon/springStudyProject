3055e85c2adaea811fc0e44ca16afd76aa504c67
ingyu <ansdlsrb1121@naver.com>
2024-02-15 11:54:58 +0900

    feat: MainController /visit 구현

--- src/main/java/ingyu/studyproject/controller/MainController.java
+++ src/main/java/ingyu/studyproject/controller/MainController.java
@@ -1,17 +1,27 @@
 package ingyu.studyproject.controller;
 
+import ingyu.studyproject.service.CustomUserDetailsService;
+import ingyu.studyproject.service.VisitService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import java.util.Collection;
 import java.util.Iterator;
 
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class MainController {
+
+    private final CustomUserDetailsService customUserDetailsService;
+    private final VisitService visitService;
     @GetMapping("/")
     public String mainP(Model model){
 
@@ -21,10 +31,26 @@ public class MainController {
         Iterator<? extends GrantedAuthority> iter = authorities.iterator();
         GrantedAuthority auth = iter.next();
         String role = auth.getAuthority();
+        
 
 
         model.addAttribute("id",id);
         model.addAttribute("role",role);
         return "main";
     }
+
+    @GetMapping("/visit")
+    public String pageViews(HttpServletRequest request,Model model){
+        String ipAddr = request.getRemoteAddr();
+        log.info("ip 주소 : {}",ipAddr);
+
+        visitService.newVisitor(ipAddr);
+        Long totalVisitor = visitService.getTotalVisitor();
+
+        model.addAttribute("total",totalVisitor);
+        return "visit";
+    }
+
+
+
 }
