9abd7bf21685c4d86a5ed6e7be0a1dd9a207e382
InGyu Moon <ansdlsrb1121@naver.com>
2024-02-25 22:44:45 +0900

    fix: UserEntity Serializable 츄ã 추가

--- src/main/java/ingyu/studyproject/entity/UserEntity.java
+++ src/main/java/ingyu/studyproject/entity/UserEntity.java
@@ -1,15 +1,23 @@
 package ingyu.studyproject.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @Entity
 @Getter
 @NoArgsConstructor
 @Table(name = "MEMBER")
-public class UserEntity {
+public class UserEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "USER_ID")
